6c25aacac48e15dfdf745f0c0e7b57842cb9cc23
Apham130 <andypham4747@gmail.com>
2025-10-23 18:02:08 +0000

    done

--- Main.java
+++ Main.java
@@ -4,6 +4,34 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a word: ");
+    String str = sc.nextLine();
+
+    String mode = "";
+    int numV = Integer.MIN_VALUE;
+    for (int i = 0; i < str.length(); i++)
+    {
+      String target = str.substring(i, i+1);
+      int num = 0;
+      for (int j = 0; j < str.length(); j++)
+      {
+        String currentLetter = str.substring(j, j+1);
+        if (currentLetter.equals(target))
+        {
+          num++;
+        }
+      }
+      if (num > numV) {
+        mode = target;
+        numV = num;
+      }
+    }
+    System.out.println("The letter " + mode + " appears the most times.");
+
+
+
+    sc.close();
   }
 }
